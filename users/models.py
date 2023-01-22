56fc99cece16ff51ffeb7541e2a5ddb2dc4bb635
Yousef <yousefghazal2412@gmail.com>
2023-01-22 17:35:35 +0200

    user model

---
+++ users/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from django.utils.translation import gettext_lazy as _
+
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        email = email.lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(_("email address"), unique=True)
+    temp_password = models.CharField(max_length=100, null=True, blank=True)
+
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_superuser = models.BooleanField(
+        "Superuser status",
+        default=False,
+        help_text="Designates that this user has all permissions without explicitly assigning them.",
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+        help_text="Designates whether this user should be treated as active. "
+        "Unselect this instead of deleting accounts.",
+    )
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    @property
+    def username(self):
+        return self.email
+
+    def get_username(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+
+class AccountAddress(models.Model):
+    name = models.CharField(max_length=150, blank=True)
+    country = models.CharField(max_length=150, blank=True)
+    state = models.CharField(max_length=150, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    address = models.CharField(max_length=300, blank=True)
+    phone_number = models.IntegerField(max_length=20, blank=True)
