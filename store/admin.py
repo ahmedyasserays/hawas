93a7e4a69719e6df41b35db483eddba946c07330
m7mdGNO <m7mdelganainy2001@gmail.com>
2023-01-26 04:20:22 +0000

    models with admin and env in admin

---
+++ store/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin, OrderedTabularInline
+from . import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name","name_ar"]
+    search_fields = ['name','name_ar']
+    
+    
+    
+class OrderOptionsInline(OrderedTabularInline):
+    model = models.Option
+    extra = 1
+    fields = ["name", "name_ar", "quantity", "price", "discount", "move_up_down_links"]
+    readonly_fields = ["move_up_down_links"]
+    ordering = ("order",)
+
+
+
+class ImagesInline(admin.StackedInline):
+    model = models.ProductImage
+    extra = 1
+    
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "name_ar", "slug", "available", "created", "updated"]
+    list_filter = ["brand", "category", "available", "created", "updated"]
+    list_editable = ["available"]
+    search_fields = ["name", "name_ar", "slug"]
+    readonly_fields = ["slug", "created", "updated", "visits"]
+    filter_horizontal = ["related_products"]
+    inlines = [OrderOptionsInline, ImagesInline]
+
+    def get_urls(self):
+        return self.inlines[0](models.Product, self.admin_site).get_urls() + super().get_urls()
+
+
+@admin.register(models.ProductReview)
+class ModelReviewAdmin(admin.ModelAdmin):
+    list_display = ["user","product"]
---
+++ store/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from autoslug.fields import AutoSlugField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from ordered_model.models import OrderedModel
+from django.contrib.auth import get_user_model
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    name_ar = models.CharField(max_length=150)
+    slug = AutoSlugField(max_length=100, unique=True, populate_from="name")
+    img = models.ImageField(blank=True)
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    name_ar = models.CharField(max_length=150)
+
+    slug = AutoSlugField(
+        max_length=100, unique=True, populate_from="name", db_index=True
+    )
+    
+    describtion = models.TextField(max_length=1000)
+    describtion_ar = models.TextField(max_length=1000)
+
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    related_products = models.ManyToManyField(
+        "Product", related_name="parents", blank=True
+    )
+    
+    visits = models.PositiveIntegerField(default=0)
+    
+    class Meta:
+        ordering = ["-created"]
+        
+
+    def __str__(self):
+        return self.name
+    
+    
+class Option(OrderedModel):
+    name = models.CharField(max_length=150)
+    name_ar = models.CharField(max_length=150)
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="options"
+    )
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField()
+    discount = models.FloatField(
+        validators=[
+            MaxValueValidator(100, "Can't make discount more than 100%"),
+            MinValueValidator(0, "Can't create offer less than 0%"),
+        ],
+        default=0,
+    )
+
+    order_with_respect_to = "product"
+    
+    def __str__(self) -> str:
+        return f"{self.product.name} | {self.name}"
+
+    @property
+    def price_after_discount(self):
+        return self.price - (self.price * (self.discount / 100))
+
+
+
+class ProductImage(models.Model):
+    img = models.ImageField()
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
+
+
+
+class ProductReview(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rate = models.PositiveIntegerField(
+        validators=[MaxValueValidator(5, "Can't rate product with more than 5 starts")]
+    )
+    comment = models.TextField()
+
+    class Meta:
+        unique_together = ["user", "product"]
