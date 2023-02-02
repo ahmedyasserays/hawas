1eda442edcda40262f3e2894f4427993623acd45
m7mdGNO <m7mdelganainy2001@gmail.com>
2023-02-02 20:41:37 +0000

    fix marksafe url in category admin page

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin, OrderedTabularInline
 from . import models
 from django.db.models import Sum,Count
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 
 
@@ -9,6 +11,7 @@ from django.db.models import Sum,Count
 class CategoryAdmin(OrderedModelAdmin):
     list_display = ["name","name_ar","number_of_products","products_of_this_category","move_up_down_links", "order"]
     search_fields = ['name','name_ar']
+    # list_display_links = ["products_of_this_category"]
     
     def number_of_products(self, obj):
         query_set = models.Category.objects.filter(id=obj.id).aggregate(Count("products"))
@@ -16,7 +19,9 @@ class CategoryAdmin(OrderedModelAdmin):
     
     def products_of_this_category(self,obj):
         link = f"http://127.0.0.1:8000/admin/store/product/?category__id__exact={obj.id}"
-        return link
+        return mark_safe(u' <a href="http://127.0.0.1:8000/admin/store/product/?category__id__exact=%s" >show products</a> ' % obj.id)
+
+    products_of_this_category.allow_tags = True
     
     
 class ProductOptionsInline(OrderedTabularInline):
